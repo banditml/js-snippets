793061d9459bec5b6dc9e9c683a2796816717b05
Lionel Vital <lgvital@gmail.com>
2020-03-13 03:02:41 -0700

    Modify endpoint to handle callbacks and separate logDecision call

--- static/js/banditMl.js
+++ static/js/banditMl.js
@@ -47,6 +47,7 @@ function BanditAPI (apiKey) {
   const banditHostUrl = "http://localhost:8000/api/";
   this.banditDecisionEndpoint = banditHostUrl + "decision";
   this.banditLogRewardEndpoint = `${banditHostUrl}reward`;
+  this.banditLogDecisionEndpoint = `${banditHostUrl}decision/log`;
 
   // URLs & hosts
   this.ipUrl = "https://api.ipify.org?format=json";
@@ -70,8 +71,8 @@ BanditAPI.prototype.asyncGetRequest = async function(
   }
 
   const response = await fetch(url, {
-      method: 'GET',
-      headers: headersDict
+    method: 'GET',
+    headers: headersDict
   });
   return await response.json();
 };
@@ -94,84 +95,140 @@ BanditAPI.prototype.asyncPostRequest = async function postData(
 };
 
 BanditAPI.prototype.getContext = function() {
-    return JSON.parse(this.storage.getItem(this.contextName));
+  return JSON.parse(this.storage.getItem(this.contextName));
 };
 
 BanditAPI.prototype.setContext = function(dict) {
-    this.storage.setItem(this.contextName, JSON.stringify(dict));
+  this.storage.setItem(this.contextName, JSON.stringify(dict));
 };
 
 BanditAPI.prototype.clearContext = function() {
-    this.storage.removeItem(this.contextName);
+  this.storage.removeItem(this.contextName);
 };
 
 // TODO: merge with updateContextValue and just have one updateContext?
 BanditAPI.prototype.updateContextList = function(newContext) {
-    // for variable length features use this method
-    // i.e. productsInCart = [12, 1, 54, ...];
-    const self = this;
-    Object.keys(newContext).forEach(function(key) {
-      self.assert(
-        Array.isArray(newContext[key]),
-        `Key "${key}" is not an array.`
-      )
-    });
+  // for variable length features use this method
+  // i.e. productsInCart = [12, 1, 54, ...];
+  const self = this;
+  Object.keys(newContext).forEach(function(key) {
+    self.assert(
+      Array.isArray(newContext[key]),
+      `Key "${key}" is not an array.`
+    )
+  });
 
-    let context = self.getContext();
-    if (context == null) {
-      context = newContext;
-    } else {
-      Object.keys(newContext).forEach(function(key) {
-        if (key in context) {
-          context[key].push(...newContext[key]);
-        } else {
-          context[key] = newContext[key];
-        }
-      });
-    }
-    self.setContext(context);
+  let context = self.getContext();
+  if (context == null) {
+    context = newContext;
+  } else {
+    Object.keys(newContext).forEach(function(key) {
+      if (key in context) {
+        context[key].push(...newContext[key]);
+      } else {
+        context[key] = newContext[key];
+      }
+    });
+  }
+  self.setContext(context);
 };
 
 BanditAPI.prototype.updateContextValue = function(newContext) {
-    // for typical features (not variable length) use this method
-    // i.e. cartValue = 35.99;
-    const self = this;
-    let context = self.getContext();
-    if (context == null) {
-      context = newContext;
-    } else {
-      context = Object.assign({}, context, newContext);
-    }
-    self.setContext(context);
+  // for typical features (not variable length) use this method
+  // i.e. cartValue = 35.99;
+  const self = this;
+  let context = self.getContext();
+  if (context == null) {
+    context = newContext;
+  } else {
+    context = Object.assign({}, context, newContext);
+  }
+  self.setContext(context);
 };
 
-BanditAPI.prototype.getDecision = function(experimentId) {
-    // set the IP address before making a decision and logging context
-    let ipPromise = this.asyncGetRequest(this.ipUrl);
-    ipPromise.then(response => {
-      return response;
-    }).then(data => {
-      this.updateContextValue({ip: data.ip})
-    });
+BanditAPI.prototype.getDecision = async function (
+  experimentId,
+  productRecs = null,
+  filterRecs = null,
+  populateProductRecs = null,
+  shouldLogDecision = true
+) {
+  // set the IP address before making a decision and logging context
+  let ipPromise = this.asyncGetRequest(this.ipUrl);
+  ipPromise.then(response => {
+    return response;
+  }).then(data => {
+    this.updateContextValue({ip: data.ip})
+  });
 
-    // call gradient-app and get a decision
-    // how to know which experiment?
-    let context = this.getContext();
-    let decisionPromise = this.asyncGetRequest(
-      url = this.banditDecisionEndpoint,
-      paramName = "context",
-      body = context,
-      headers = {
-        "Authorization": `ApiKey ${this.banditApikey}`
+  // call gradient-app and get a decision
+  // how to know which experiment?
+  let context = this.getContext();
+  let decisionPromise = this.asyncGetRequest(
+    url = this.banditDecisionEndpoint,
+    paramName = "context",
+    body = context,
+    headers = {
+      "Authorization": `ApiKey ${this.banditApikey}`
+    }
+  );
+
+  return decisionPromise.then(async (response) => {
+    let decision = response.decision;
+    let productRecIds;
+    if (productRecs) {
+      if (response.isControl) {
+        // productRecs can be array of IDs
+        if (Array.isArray(productRecs)) {
+          productRecIds = productRecs;
+        } else {
+          // productRecs could also be a function that returns IDs or an async function that returns promise of IDs
+          let result = productRecs();
+          if (result && result.then) {
+            // if fn returns a promise, we await it then expect ids to be returned
+            productRecIds = await result;
+          } else {
+            productRecIds = result;
+          }
+        }
+      } else {
+        productRecIds = response.decision;
       }
-    );
+      if (filterRecs) {
+        // filterRecs can be function that directly returns IDs or promise
+        let result = filterRecs(productRecIds);
+        if (result && result.then) {
+          productRecIds = await result;
+        }
+      }
+      decision.decision = productRecIds;
+      if (populateProductRecs) {
+        let result = populateProductRecs(productRecIds);
+        if (result && result.then) {
+          await result;
+        }
+      }
+      if (shouldLogDecision) {
+        this.logDecision(context, decision);
+      }
+    }
+    return response;
+  }).catch(function(e) {
+    console.error(e);
+    return {"message": "Decision request failed."};
+  });
+};
 
-    return decisionPromise.then(response => {
-      return response;
-    }).catch(function(e) {
-      console.error(e);
-      return {"message": "Decision request failed."};
-    });
+BanditAPI.prototype.logDecision = function(context, decision) {
+  const headers = {
+    "Authorization": `ApiKey ${this.banditApikey}`
+  };
+  this.asyncPostRequest(this.banditLogDecisionEndpoint, headers, {
+    context: context,
+    decision: decision
+  }).then(response => {
+    return response;
+  });
 };
 
 BanditAPI.prototype.logReward = function(decisionId, reward) {
@@ -185,7 +242,6 @@ BanditAPI.prototype.logReward = function(decisionId, reward) {
     return response;
   });
 };
-
 
 // var bandit = new BanditAPI("0dc5dc79-95fc-3fa5-b141-9719983c32b0");
 var bandit = new BanditAPI("c52e1439-77bc-3cfd-b422-6a4d9a1e2c8c");
